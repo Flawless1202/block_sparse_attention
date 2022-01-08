61652ac1461a7ae00d1ee70025fa31a44796eb7c
Kai Chen <14sdck@tongji.edu.cn>
2022-01-09 00:26:29 +0800

    add 4x4 reg optimal for cuda matmul

--- src/matmul_cuda.cpp
+++ src/matmul_cuda.cpp
@@ -3,6 +3,8 @@
 // CUDA forward declarations
 torch::Tensor matmul_cuda(torch::Tensor a, torch::Tensor b);
 torch::Tensor matmul_shared_memory_cuda(torch::Tensor a, torch::Tensor b, const int block_size = 16);
+torch::Tensor matmul_shared_memory_4x4reg_cuda(torch::Tensor a, torch::Tensor b, const int block_size = 16);
+
 
 // C++ interface
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
@@ -23,7 +25,15 @@ torch::Tensor matmul_shared_memory(torch::Tensor a, torch::Tensor b, const int b
     return matmul_shared_memory_cuda(a, b, block_size);
 }
 
+torch::Tensor matmul_shared_memory_4x4reg(torch::Tensor a, torch::Tensor b, const int block_size = 16) {
+    CHECK_INPUT(a);
+    CHECK_INPUT(b);
+
+    return matmul_shared_memory_4x4reg_cuda(a, b, block_size);
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("matmul", &matmul, "MatMul implemented by CUDA.");
     m.def("matmul_shared_memory", &matmul_shared_memory, "MatMul implemented by CUDA with shared memory.");
+    m.def("matmul_shared_memory_4x4reg", &matmul_shared_memory_4x4reg, "MatMul implemented by CUDA with shared memory and register optimal.");
 }
--- src/matmul_cuda_kernel.cu
+++ src/matmul_cuda_kernel.cu
@@ -46,9 +46,6 @@ __global__ void matmul_shared_memory_cuda_kernel(
     const int row = blockIdx.x * blockDim.x + threadIdx.x;
     const int col = blockIdx.y * blockDim.y + threadIdx.y;
 
-    // __shared__ scalar_t _sh_tile_a[block_size][block_size];
-    // __shared__ scalar_t _sh_tile_b[block_size][block_size];
-
     extern __shared__ char data[];
     scalar_t* _sh_tile_a = (scalar_t*)data;
     scalar_t* _sh_tile_b = (scalar_t*)(block_size * block_size * sizeof(scalar_t) + data);
@@ -69,6 +66,74 @@ __global__ void matmul_shared_memory_cuda_kernel(
                 __syncthreads();
             }
             out[bidx][hidx][row][col] = val;
+        }
+    }
+}
+
+template <typename scalar_t>
+__global__ void matmul_shared_memory_4x4reg_cuda_kernel(
+    const torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> a,
+    const torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> b,
+    torch::PackedTensorAccessor32<scalar_t, 4, torch::RestrictPtrTraits> out,
+    const int num_heads,
+    const int m,
+    const int n,
+    const int k,
+    const int block_size) {
+    const int bidx = blockIdx.z;
+    const int tile_row = blockIdx.x * blockDim.x + threadIdx.x;
+    const int tile_col = blockIdx.y * blockDim.y + threadIdx.y;
+
+    extern __shared__ char data[];
+    scalar_t* _sh_tile_a = (scalar_t*)data;
+    scalar_t* _sh_tile_b = (scalar_t*)(block_size * block_size * sizeof(scalar_t) + data);
+
+    const int num_blocks = (k + block_size - 1) / block_size;
+
+    if (tile_row < m / 4 && tile_col < n / 4) {
+        for (int hidx = 0; hidx < num_heads; hidx++) {
+            scalar_t val[4][4] = {0.0};
+            scalar_t reg_a[4] = {0.0};
+            scalar_t reg_b[4] = {0.0};
+            for (int i = 0; i < num_blocks; i++) {
+            #pragma unroll
+                for (int _tm = 0; _tm < 4; _tm++) {
+                #pragma unroll
+                    for (int _tn = 0; _tn < 4; _tn ++) {
+                        const int _trow = threadIdx.x * 4 + _tm;
+                        const int _tcol = threadIdx.y * 4 + _tn;
+                        _sh_tile_a[_trow * block_size + _tcol] = a[bidx][hidx][tile_row * 4 + _tm][i * block_size + _tcol];
+                        _sh_tile_b[_trow * block_size + _tcol] = b[bidx][hidx][i * block_size + _trow][tile_col * 4 + _tn];
+                    }
+                }
+                __syncthreads();
+
+                for (int j = 0; j < block_size; j++) {
+                #pragma unroll
+                    for (int _tm = 0; _tm < 4; _tm++) {
+                        const int _trow = threadIdx.x * 4 + _tm;
+                        reg_a[_tm] = _sh_tile_a[_trow * block_size + j];
+                    }
+                #pragma unroll
+                    for (int _tn = 0; _tn < 4; _tn++) {
+                        const int _tcol = threadIdx.y * 4 + _tn;
+                        reg_b[_tn] = _sh_tile_b[j * block_size + _tcol];
+                    }
+                #pragma unroll
+                    for (int _tm = 0; _tm < 4; _tm++) {
+                        for (int _tn = 0; _tn < 4; _tn++) {
+                            val[_tm][_tn] += reg_a[_tm] * reg_b[_tn];
+                        }
+                    }
+                }
+                __syncthreads();
+            }
+        #pragma unroll
+            for (int _tm = 0; _tm < 4; _tm++) {
+                for (int _tn = 0; _tn < 4; _tn++) {
+                    out[bidx][hidx][tile_row * 4 + _tm][tile_col * 4 + _tn] = val[_tm][_tn];
+                }
+            }
         }
     }
 }
@@ -133,6 +198,40 @@ torch::Tensor matmul_shared_memory_cuda(torch::Tensor a, torch::Tensor b, const 
     AT_DISPATCH_FLOATING_TYPES_AND_HALF(
         a.type(), "matmul_shared_memory_cuda", ([&] {
             matmul_shared_memory_cuda_kernel<scalar_t>
+                <<<blocks_per_grid, threads_per_block, 2 * block_size * block_size * sizeof(scalar_t)>>>(
+                    a.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(),
+                    b.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(),
+                    out.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(), h, ma, nb, ka, block_size);
+        }));
+    return out;
+}
+
+torch::Tensor matmul_shared_memory_4x4reg_cuda(torch::Tensor a, torch::Tensor b, const int block_size = 16) {
+    const int bs = a.size(0);
+    const int h = a.size(1);
+
+    // the tensor a is of size `(bs, h, ma, ka)`
+    const int ma = a.size(-2);
+    const int ka = a.size(-1);
+
+    // the tensor b is of size `(bs, h, kb, nb)`
+    const int kb = b.size(-2);
+    const int nb = b.size(-1);
+
+    if (ka != kb) {
+        throw std::invalid_argument("Size of tensor A must match size of tensor B.");
+    }
+
+    // configure cuda
+    const dim3 threads_per_block(block_size / 4, block_size / 4, 1);
+    const dim3 blocks_per_grid(ma / block_size + 1, nb / block_size + 1, bs);
+
+    auto tensor_options = torch::TensorOptions().dtype(a.dtype()).device(torch::kCUDA, a.device().index());
+    auto out = torch::zeros({bs, h, ma, nb}, tensor_options);
+
+    AT_DISPATCH_FLOATING_TYPES_AND_HALF(
+        a.type(), "matmul_shared_memory_4x4reg_cuda", ([&] {
+            matmul_shared_memory_4x4reg_cuda_kernel<scalar_t>
                 <<<blocks_per_grid, threads_per_block, 2 * block_size * block_size * sizeof(scalar_t)>>>(
                     a.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(),
                     b.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(),
